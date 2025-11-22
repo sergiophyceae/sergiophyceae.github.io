abb7520e5b1bb7c4c3d2f83408a7743a378d9e5c
sergiophyceae <36368696+sergiophyceae@users.noreply.github.com>
2025-11-22 12:41:17 -0500

    Update external-posts.rb

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -25,7 +25,12 @@ module ExternalPosts
     def fetch_from_rss(site, src)
       xml = HTTParty.get(src['rss_url']).body
       return if xml.nil?
-      feed = Feedjira.parse(xml)
+      begin
+        feed = Feedjira.parse(xml)
+        rescue Feedjira::NoParserAvailable
+        Jekyll.logger.warn "ExternalPosts:", "No valid parser for RSS at #{src['rss_url']}"
+        return
+      end
       process_entries(site, src, feed.entries)
     end
 
